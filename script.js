13e68fd5ef50c7197288d54d2648627b27addc73
mallex <lexyshipanga@gmail.com>
2026-08-31 17:29:52 +0200

    Wrap workerSrc assignment in a conditional check

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+if (typeof pdfjsLib !== 'undefined') {
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+}
 
 const defaultPortfolioData = {
   "projects": [
